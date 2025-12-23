896cfa556cc29d2a041c7158fb1065af2c1a24ec
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-12-23 16:53:54 -0300

    startingo to change for redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react'
+import { Provider } from 'react-redux'
+
 import Header from './components/Header'
 import Produtos from './containers/Produtos'
+import { store } from './store'
 
 import { GlobalStyle } from './styles'
 
@@ -16,7 +19,6 @@ export type Game = {
 
 function App() {
   const [games, setGames] = useState<Game[]>([])
-  const [carrinho, setCarrinho] = useState<Game[]>([])
 
   useEffect(() => {
     fetch('http://localhost:4000/produtos')
@@ -24,22 +26,18 @@ function App() {
       .then((res) => setGames(res))
   }, [])
 
-  function adicionarAoCarrinho(jogo: Game) {
-    if (carrinho.find((game) => game.id === jogo.id)) {
-      alert('Item já adicionado')
-    } else {
-      setCarrinho([...carrinho, jogo])
-    }
+  function adicionarAoCarrinho() {
+    console.log()
   }
 
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <div className="container">
-        <Header itensNoCarrinho={carrinho} />
+        <Header />
         <Produtos jogos={games} adicionarAoCarrinho={adicionarAoCarrinho} />
       </div>
-    </>
+    </Provider>
   )
 }
 
