25f928bb857a2a1ebed2efb265958be85790b4cc
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-12-27 10:12:34 -0300

    using Redux toolkit query for api calling

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,17 +1,16 @@
 import { Game } from '../App'
 import Produto from '../components/Produto'
+import { useGetJogosQuery } from '../services/api'
 
 import * as S from './styles'
 
-type Props = {
-  jogos: Game[]
-}
-
-const Produtos = ({ jogos }: Props) => {
+const Produtos = () => {
+  const { data: jogos, isLoading } = useGetJogosQuery()
+  if (isLoading) return <h2>Carregando...</h2>
   return (
     <>
       <S.Produtos>
-        {jogos.map((game) => (
+        {jogos?.map((game) => (
           <Produto key={game.id} game={game} />
         ))}
       </S.Produtos>
